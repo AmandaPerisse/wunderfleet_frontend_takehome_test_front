58522314e7e2007a6b4ddc8b1e32b656462b4e55
Amanda <amandaveras1999@gmail.com>
2022-06-30 22:52:39 -0300

    Update files

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import SignUpPage from './pages/SignUpPage';
 
 function App() {
 
-  const initialToken = localStorage.getItem("token");
-  const initialUsername = localStorage.getItem("username");
-  const initialUserStreet = localStorage.getItem("street");
-  const initialUserNumber = localStorage.getItem("number");
-
-  const [infoUsername, setInfoUsername] = useState(JSON.parse(initialUsername));
-  const [infoStreet, setInfoStreet] = useState(JSON.parse(initialUserStreet));
-  const [infoNumber, setInfoNumber] = useState(JSON.parse(initialUserNumber));
-  const [token, setToken] = useState(initialToken);
-
-  const [cartAmount, setCartAmount] = React.useState(0);
-
   return (
 
     <UserContext.Provider value={{ infoNumber, setInfoNumber, infoUsername, setInfoUsername, infoStreet, setInfoStreet, token, setToken }} >
